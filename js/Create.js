a840c583044918e934386f8a1f885bb2434dea6d
Tedman Hsu <tedh526@gmail.com>
2017-01-26 17:13:30 -0500

    stuff kind of works

--- js/Create.js
+++ js/Create.js
@@ -3,7 +3,7 @@ var walls;
 var cursors;
 var wasd;
 var bullets;
-var fireRate = 100;
+var fireRate = 400;
 var nextFire = 0;
 
 
@@ -28,8 +28,8 @@ function create() {
     player.scale.setTo(1);
     player.animations.add('left', [22, 23, 24, 25, 26, 27, 28], 10, true);
     player.animations.add('right', [33, 34, 35, 36, 37, 38, 39], 10, true);
-    player.animations.add('down', [0, 1, 2, 3, 4, 5, 6], true);
-    player.animations.add('up', [11, 12, 13, 14, 15, 16, 17], true);
+    player.animations.add('down', [0, 1, 2, 3, 4, 5, 6], 10, true);
+    player.animations.add('up', [11, 12, 13, 14, 15, 16, 17], 10, true);
     game.camera.follow(player);
     game.physics.arcade.enable(player);
     player.body.collideWorldBounds = true;
@@ -43,7 +43,6 @@ function create() {
     bullets.setAll('anchor.y', 0.5);
     bullets.setAll('checkWorldBounds', true);
     bullets.setAll('outOfBoundsKill', true);
-
 
 
     //  Our controls.
--- js/Update.js
+++ js/Update.js
@@ -44,31 +44,69 @@ function update() {
         player.frame = 0;
     }
 
-    function fire() {
+
+function fire(direction) {
         if (game.time.now > nextFire && bullets.countDead() > 0) {
-            nextFire = game.time.now + fireRate;
-            var bullet = bullets.getFirstDead();
-            bullet.reset(player.x - 8, player.y - 8);
-
-            game.physics.arcade.moveToPointer(bullet, 300);
+                nextFire = game.time.now + fireRate;
+                var bullet = bullets.getFirstDead();
+                bullet.scale.setTo(0.5);
+                bullet.reset(player.x, player.y);
+                switch(direction) {
+                    case 'left' : game.physics.arcade.moveToXY(bullet, -1000, player.y, 500); break;
+                    case 'right': game.physics.arcade.moveToXY(bullet, 1000, player.y, 500); break;
+                    case 'up': game.physics.arcade.moveToXY(bullet, player.x, -1000, 500); break;
+                    case 'down': game.physics.arcade.moveToXY(bullet, player.x, 1000, 500); break;
+                    case 'up-left': game.physics.arcade.moveToXY(bullet, player.x - 1000, player.y - 1000, 500); break;
+                    case 'up-right': game.physics.arcade.moveToXY(bullet, player.x + 1000, player.y - 1000, 500); break;
+                    case 'down-left': game.physics.arcade.moveToXY(bullet, player.x - 1000, player.y + 1000, 500); break;
+                    case 'down-right': game.physics.arcade.moveToXY(bullet, player.x + 1000, player.y + 1000, 500); break;
+                }
         }
-    }
+}
+    // function fire() {
+    //     if (game.time.now > nextFire && bullets.countDead() > 0) {
+    //         nextFire = game.time.now + fireRate;
+    //         var bullet = bullets.getFirstDead();
+    //         bullet.scale.setTo(0.5);
+    //         console.log('player', player.x, player.y)
+    //         bullet.reset(player.x - 8, player.y - 8);
 
+    //         game.physics.arcade.moveToPointer(bullet, 300);
+    //         console.log('bullet', bullet.x, bullet.y)
+    //     }
+    // }
 
-    if (cursors.left.isDown)
+    if (cursors.up.isDown && cursors.left.isDown)
     {
-        fire();
+        fire('up-left');
+    }
+    else if (cursors.up.isDown && cursors.right.isDown)
+    {
+        fire('up-right');
+    }
+    else if (cursors.down.isDown && cursors.left.isDown)
+    {
+        fire('down-left');
+    }
+    else if (cursors.down.isDown && cursors.right.isDown)
+    {
+        fire('down-right');
+    }
+    else if (cursors.left.isDown)
+    {
+        fire('left');
     }
     else if (cursors.right.isDown)
     {
-        fire();
+        fire('right');
     }
     else if (cursors.up.isDown)
     {
-        fire();
+        fire('up');
     }
     else if (cursors.down.isDown)
     {
-        fire();
+        fire('down');
     }
+
 }
